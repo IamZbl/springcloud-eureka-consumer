30d968d9705ff21e0e4972d68633025818d98009
IamZbl <475749368@qq.com>
2018-08-26 22:05:55 +0800

    熔断机制

--- src/main/java/com/bigdog/consumer/HelloRemote.java
+++ src/main/java/com/bigdog/consumer/HelloRemote.java
@@ -4,8 +4,11 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bigdog.hystrix.HelloRemoteHystrix;
+
 //name远程服务名，即spring.application.name=springcloud-eureka-client
-@FeignClient(name = "springcloud-eureka-client")
+//添加指定fallback类，在服务熔断的时候返回fallback类中的内容。
+@FeignClient(name = "springcloud-eureka-client",fallback=HelloRemoteHystrix.class)
 public interface HelloRemote {
 
 	@RequestMapping(value = "/hello")
